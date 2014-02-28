9710aa9c5c10546f91c0bf75bf230b7dfc9cd695
kagami-c <kagami.cathez@gmail.com>
2014-02-28 20:33:44 +0800

    Find the slowest proc -> response.read()
    running in windows 7 & py 3.3 & urllib.request is slower than running in
    Ubuntu & py 2.7 & urllib2 in virtual machine !

--- dmhy_crawler_windows_ver.py
+++ dmhy_crawler_windows_ver.py
@@ -9,20 +9,31 @@ def get_page(webaddr):
 	}
 	req = urllib.request.Request(url=webaddr, headers=headers)
 	response = urllib.request.urlopen(req)
+
+	# speed test
+	print("Get response.")
+
+	# read method is the slowest proc
 	page = response.read().decode("utf-8")
+	
+	# speed test
+	print("Finish read.")
 
 	return page
 
-def get_tbody(page):
-	delete_newline = re.compile(r"\n")
-	delte_return = re.compile(r"\r")
-	match_tbody = re.compile(r".*<tbody>(.*)</tbody>.*")
 
+# get_tbody regular expression
+delete_newline = re.compile(r"\n")
+delte_return = re.compile(r"\r")
+match_tbody = re.compile(r".*<tbody>(.*)</tbody>.*")
+# get_tbody function
+def get_tbody(page):
 	line1 = delete_newline.sub(r' ', page)
 	line2 = delte_return.sub(r' ', line1)
 	tbody = match_tbody.sub(r'\1', line2)
 	
 	return tbody
+
 
 def get_tr_s(tbody):
 	array = tbody.split(r'</tr>')
@@ -31,8 +42,11 @@ def get_tr_s(tbody):
 	#	print '!!!\n' + x + '\n'
 	return array
 
+
+# get_title regular expression
+tag = re.compile(r'.*<td class="title">(.*?)</td>.*')
+# get_title function
 def get_title(tr_s):
-	tag = re.compile(r'.*<td class="title">(.*?)</td>.*')
 	title_array = []
 	for each in tr_s:
 		title_array.append(tag.sub(r'\1', each))
@@ -41,9 +55,12 @@ def get_title(tr_s):
 	#	print '!!!' + x + '\n'
 	return title_array
 
+
+# get_href regular expression
+get_a = re.compile(r'.*(<a href.*?</a>).*?$')
+add_addr = re.compile(r'href="')
+# get_href function
 def get_href(title_array):
-	get_a = re.compile(r'.*(<a href.*?</a>).*?$')
-	add_addr = re.compile(r'href="')
 	ref_array = []
 	for each in title_array:
 		temp = get_a.sub(r'\1', each)
@@ -53,6 +70,9 @@ def get_href(title_array):
 	#	print x + '\n'
 	return ref_array
 
+
+# the main proc
+#print("Finished re.compile")
 if __name__ == '__main__':
 		
 	htmlfile = codecs.open('htmlfile.html', 'w', 'utf-8')
@@ -67,10 +87,17 @@ if __name__ == '__main__':
 
 	start = 1
 	increment = 1
-	#end = 142
-	end = 1 # just for test
+	end = 31
+	#end = 1 # just for test
 	
 	base = 'http://share.dmhy.org/topics/list/page/'
+
+	# content of share.dmhy.org/robots.txt below
+	#
+	# User-agent: *
+	# Disallow: /topics/down/
+	# Disallow: /index.php/topics/down/
+	# Allow: /
 
 	while(start <= end):
 		
@@ -83,8 +110,14 @@ if __name__ == '__main__':
 		title_array = get_title(tr_s)
 		ref_array = get_href(title_array)
 		
+		# speed test
+		#print("Finish parse works.")
+
 		for s in ref_array:
 			htmlfile.write(s + '<br />\n\r')
+
+		# speed test
+		#print("Write end.")
 
 		time.sleep(0.1)
 		start += increment
